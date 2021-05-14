4021d742e09b4812329bfe0fa7236f39889c3f03
Aline Galdino <46880720+Allien01@users.noreply.github.com>
2021-05-14 11:21:46 -0300

    Delete basics directory

--- basics/Loops/02.py
+++
@@ -1,7 +0,0 @@
-mini = None
-numbers = input("Enter a list: ").split(" ")
-for num in numbers:
-    n = int(num)
-    if mini is None or n < mini:
-        mini = n
-print("Menor:", mini)
--- basics/Loops/03.py
+++
@@ -1,7 +0,0 @@
-major = None
-numbers = input("Enter a list: ").split(" ")
-for num in numbers:
-    n = int(num)
-    if major is None or n > major:
-        major = n
-print("Maior:", major)
